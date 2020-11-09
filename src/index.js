59f92d732299ef833874005e8cb81eb413e28745
Anthony Lin <anthony.lin.198@gmail.com>
2020-11-09 14:06:20 -0800

    taskslist

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 // import App from "./projects/TodoReact/props";
 // import App from "./projects/AsyncAwait/App";
-import App from "./projects/Movie/Props";
+import App from "./projects/TasksList/Props";
 
 ReactDOM.render(
   <React.StrictMode>
