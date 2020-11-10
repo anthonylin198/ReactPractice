fdc4570e4b23f71f6af3ee9dc41e638b3ff2fd62
Anthony Lin <anthony.lin.198@gmail.com>
2020-11-09 16:36:22 -0800

    index update

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 // import App from "./projects/TodoReact/props";
 // import App from "./projects/AsyncAwait/App";
-// import App from "./projects/TasksList/Props";
+import App from "./projects/TasksList/Props";
 // import App from "./projects/TasksList/Redux";
-import App from "./projects/Redux/App";
+// import App from "./projects/Redux/App";
 
 ReactDOM.render(
   <React.StrictMode>
