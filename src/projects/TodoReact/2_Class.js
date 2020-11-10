9e7bfa86dc84923d0fcf73476e0992257722bfd8
Anthony Lin <anthony.lin.198@gmail.com>
2020-11-10 09:57:20 -0800

    class components todo

--- src/projects/TodoReact/2_Class.js
+++ src/projects/TodoReact/2_Class.js
@@ -4,18 +4,25 @@ import React from "react";
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { input: "", list: [1, 2, 3, 4] };
-    this.handleInput = this.handleInput.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-  handleInput(e) {
-    this.setState({ input: e.target.value });
+    this.state = { input: "", list: [] };
+    this.handleSubmit = this.handleSubmit.bind(this); // need to bind when drilling
+    this.handleDelete = this.handleDelete.bind(this);
+    this.handleUpdate = this.handleUpdate.bind(this);
   }
   handleSubmit(input) {
     const newList = this.state.list.slice();
-    console.log(newList);
     newList.push(input);
     this.setState({ input, list: newList });
+  }
+  handleDelete(index) {
+    const newList = this.state.list.slice();
+    newList.splice(index, 1);
+    this.setState({ list: newList });
+  }
+  handleUpdate(index, input) {
+    const newList = this.state.list.slice();
+    newList[index] = input;
+    this.setState({ list: newList });
   }
   render() {
     return (
@@ -26,19 +33,30 @@ class App extends React.Component {
           handleInput={this.handleInput}
           handleSubmit={this.handleSubmit}
         />
-        <List list={this.state.list} />
+        <List
+          list={this.state.list}
+          handleDelete={this.handleDelete}
+          handleUpdate={this.handleUpdate}
+        />
       </div>
     );
   }
 }
 
 class UserInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { input: "" };
+  }
+  handleInput(e) {
+    console.log(this.state);
+    this.setState({ input: e.target.value });
+  }
   render() {
-    console.log(this.props);
     return (
       <div>
-        <input onChange={(e) => this.props.handleInput(e)}></input>
-        <button onClick={() => this.props.handleSubmit(this.props.input)}>
+        <input onChange={(e) => this.handleInput(e)}></input>
+        <button onClick={() => this.props.handleSubmit(this.state.input)}>
           Submit
         </button>
       </div>
@@ -48,14 +66,50 @@ class UserInput extends React.Component {
 
 class List extends React.Component {
   render() {
-    return (
-      <div>
-        <h1>List</h1>
-      </div>
-    );
+    const listArr = this.props.list.map((item, i) => {
+      return (
+        <ListItem
+          item={item}
+          index={i}
+          key={i}
+          handleDelete={this.props.handleDelete}
+          handleUpdate={this.props.handleUpdate}
+        />
+      );
+    });
+    return <div>{listArr}</div>;
   }
 }
 
 // create a listItem
+class ListItem extends React.Component {
+  // keep local state for the input
+  constructor(props) {
+    super(props);
+    this.state = { input: "" };
+  }
+  render() {
+    return (
+      <div className="itemContainer">
+        <span className="item">{this.props.item}</span>
+        <div className="buttonContainer">
+          <input
+            onChange={(e) => this.setState({ input: e.target.value })}
+          ></input>
+          <button
+            onClick={() =>
+              this.props.handleUpdate(this.props.index, this.state.input)
+            }
+          >
+            Update
+          </button>
+          <button onClick={() => this.props.handleDelete(this.props.index)}>
+            X
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
