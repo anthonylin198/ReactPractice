cd69c3f55c2dda0631a25b4c05976d7ea801d9b4
Anthony Lin <anthony.lin.198@gmail.com>
2020-12-07 11:10:11 -0800

    update

--- src/index.js
+++ src/index.js
@@ -11,11 +11,14 @@ import ReactDOM from "react-dom";
 // import App from "./projects/TasksList/4_Redux";
 // import App from "./projects/TasksList/5_ReduxToolkit";
 // import App from "./projects/TasksList/6_ReduxToolkitSelector";
-import App from "./projects/TasksList/7_Practice";
+// import App from "./projects/TasksList/7_Practice";
 
 // todo: ASYNC APP
 // import App from "./projects/AsyncAwait/App";
 // import App from "./projects/AsyncAwait/8_Redux/App";
+
+// todo: GRAPHQL 00 look at the config
+import App from "./projects/SpaceXGraphQL/SpaceX";
 
 ReactDOM.render(
   <React.StrictMode>
