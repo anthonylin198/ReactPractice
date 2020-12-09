5509d6dbd69cfc7709b630116344f7960a51806a
Anthony Lin <anthony.lin.198@gmail.com>
2020-12-09 11:25:06 -0800

    fixup dragdrop

--- src/projects/CssAnimations/3_DragDrop/index.js
+++ src/projects/CssAnimations/3_DragDrop/index.js
@@ -1,13 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
 
 const App = () => {
+  const [list, setList] = useState([
+    ["item1", "item2", "item3", "item4"],
+    [],
+    ["item5"],
+  ]);
+
+  console.log(list);
+
+  const notStartedList = list[0].map((item, i) => {
+    return <div key={i}>{item}</div>;
+  });
+  const inProgressList = list[1].map((item, i) => {
+    return <div key={i}>{item}</div>;
+  });
+  const completedList = list[2].map((item, i) => {
+    return <div key={i}>{item}</div>;
+  });
   return (
     <div className="container">
       <div className="boxesContainer">
-        <div className="BoxContainer">Not Completed</div>
-        <div className="BoxContainer">In Progress</div>
-        <div className="BoxContainer">Completed</div>
+        <div className="BoxContainer">
+          <h1>Not Started</h1>
+          <div>{notStartedList}</div>
+        </div>
+        <div className="BoxContainer">
+          <h1>In Progress</h1>
+          <div>{inProgressList}</div>
+        </div>
+        <div className="BoxContainer">
+          <h1>Completed</h1>
+          <div>{completedList}</div>
+        </div>
       </div>
     </div>
   );
